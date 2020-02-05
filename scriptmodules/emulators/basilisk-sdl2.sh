d4ee791cf23acade393f1a973c07cac69a94c53f
cmitu <31816814+cmitu@users.noreply.github.com>
2020-02-05 12:13:08 +0200

    basiliskII: add a SDL2 variant for the scriptmodule

---
+++ scriptmodules/emulators/basilisk-sdl2.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="basilisk-sdl2"
+rp_module_desc="Macintosh emulator - SDL2 version"
+rp_module_help="ROM Extensions: .img .rom\n\nCopy your Macintosh roms mac.rom and disk.img to $romdir/macintosh"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/kanjitalk755/macemu/master/BasiliskII/COPYING"
+rp_module_section="exp"
+rp_module_flags=""
+
+function depends_basilisk-sdl2() {
+    local depends=(libsdl2-dev autoconf automake oss-compat)
+    isPlatform "x11" && depends+=(libgtk2.0-dev)
+    getDepends "${depends[@]}"
+}
+
+function sources_basilisk-sdl2() {
+    gitPullOrClone "$md_build" https://github.com/kanjitalk755/macemu.git
+}
+
+function build_basilisk-sdl2() {
+    cd BasiliskII/src/Unix
+    local params=(--enable-sdl-video --enable-sdl-audio --disable-vosf --without-mon --without-esd)
+    ! isPlatform "x86" && params+=(--disable-jit-compiler)
+    ! isPlatform "x11" && params+=(--without-x --without-gtk)
+    ./autogen.sh --prefix="$md_inst" "${params[@]}"
+    make clean
+    make
+    md_ret_require="$md_build/BasiliskII/src/Unix/BasiliskII"
+}
+
+function install_basilisk-sdl2() {
+    cd "BasiliskII/src/Unix"
+    make install
+}
+
+function configure_basilisk-sdl2() {
+    local params=()
+
+    mkRomDir "macintosh"
+    touch "$romdir/macintosh/Start.txt"
+
+    mkUserDir "$md_conf_root/macintosh"
+
+    addEmulator 0 "$md_id" "macintosh" "$md_inst/bin/BasiliskII --rom $romdir/macintosh/mac.rom --disk $romdir/macintosh/disk.img --extfs $romdir/macintosh --config $md_conf_root/macintosh/basiliskii-sdl2.cfg ${params[*]}"
+    addSystem "macintosh"
+}
