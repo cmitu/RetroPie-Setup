e95ab9e33209432b1a13320ea0f66c79094f451b
cmitu <31816814+cmitu@users.noreply.github.com>
2026-09-06 07:54:36 +0000

    lr-hatari: fix core build due to upstream changes
    Upstream started incorporating the latest Hatari version into the core and renamed the previouos core version to `hatari2014`.
    Until the merge is complete and stable, use the old version, now in a separate branch.
    
    Modified the build to:
     - use the CAPSIMG variables that are now part of the upstream `makefile`, instead of using our own
     - update our patch to exclude our link/include flags for CAPSIMG in the `makefile` and fix the core name
     - use `-rpath` linker option to have the core `.so` library search for the `campsimg` library in the install folder (similar to how the standalone emulator build recipe does). This alows us to get rid of the `configure` modifications to `emulators.cfg` for setting `LD_LIBRARY_PATH`

--- scriptmodules/libretrocores/lr-hatari.sh
+++ scriptmodules/libretrocores/lr-hatari.sh
@@ -13,7 +13,7 @@ rp_module_id="lr-hatari"
 rp_module_desc="Atari emulator - Hatari port for libretro"
 rp_module_help="ROM Extensions: .st .stx .img .rom .raw .ipf .ctr .zip\n\nCopy your Atari ST games to $romdir/atarist"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/hatari/master/gpl.txt"
-rp_module_repo="git https://github.com/libretro/hatari.git master"
+rp_module_repo="git https://github.com/libretro/hatari.git hitari2014-mercurial"
 rp_module_section="exp"
 
 function depends_lr-hatari() {
@@ -30,7 +30,8 @@ function build_lr-hatari() {
     _build_libcapsimage_hatari
 
     cd "$md_build"
-    CFLAGS+=" -D__cdecl='' -I\"$md_build/src/includes/caps\" -DHAVE_CAPSIMAGE=1 -DCAPSIMAGE_VERSION=5" CAPSIMG_LDFLAGS="-L./lib -l:libcapsimage.so.5.1" make -f Makefile.libretro
+    make -f Makefile.libretro clean
+    LDFLAGS="-Wl,-rpath='$md_inst'" make -f Makefile.libretro capsimg=1 capssrc="$md_build/capsimg_source_linux_macosx" capslib="$md_build/lib" capslibname=":libcapsimage.so.5.1"
     md_ret_require="$md_build/hatari_libretro.so"
 }
 
@@ -52,11 +53,4 @@ function configure_lr-hatari() {
 
     addEmulator 1 "$md_id" "atarist" "$md_inst/hatari_libretro.so"
     addSystem "atarist"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    # add LD_LIBRARY_PATH='$md_inst' to start of launch command
-    iniConfig " = " '"' "$configdir/atarist/emulators.cfg"
-    iniGet "$md_id"
-    iniSet "$md_id" "LD_LIBRARY_PATH='$md_inst' $ini_value"
 }
